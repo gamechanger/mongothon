c7ac0f4974adca0533eb7a844055e9f38da9c849
Tom Leach <tom@gc.io>
2013-10-21 15:02:15 -0400

    v0.5.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name="Mongothon",
-    version="0.5.1",
+    version="0.5.2",
     author="Tom Leach",
     author_email="tom@leach.it",
     description="A MongoDB object-document mapping layer for Python",
