ee9376f617e2e8e05088be8ad40974f72c6086af
Tom Leach <tom@gc.io>
2013-11-22 10:52:25 -0500

    Detect circular references in dicts used to populate models
    Raise a helpful error rather than allowing a nasty stack overflow.

--- mongothon/document.py
+++ mongothon/document.py
@@ -3,22 +3,35 @@ class Document(dict):
         if initial:
             self.populate(initial)
 
-    def populate(self, from_dict):
-        """Populates this document from the given dict."""
+    def _populate(self, from_dict, seen):
+        if id(from_dict) in seen:
+            raise ValueError("Circular reference detected in dict used to populate Mongothon document")
+        seen.add(id(from_dict))
+
+        def create_doc(value):
+            doc = Document()
+            doc._populate(value, seen)
+            return doc
 
         self.clear()
         for key, value in from_dict.iteritems():
             if isinstance(value, dict):
-                self[key] = Document(value)
+                self[key] = create_doc(value)
 
             elif isinstance(value, list):
                 self[key] = []
                 for item in value:
                     if isinstance(item, dict):
-                        self[key].append(Document(item))
+                        self[key].append(create_doc(item))
                     else:
                         self[key].append(item)
 
             else:
                 self[key] = value
+
+    def populate(self, from_dict):
+        """Populates this document from the given dict."""
+        seen = set()
+        self._populate(from_dict, seen)
+
 
