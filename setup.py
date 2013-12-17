59047e8f7383e82e030526c20cd940dab7a47ec3
Kiril Savino <kiril@gamechanger.io>
2013-12-17 17:05:51 -0500

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name="Mongothon",
-    version="0.5.5",
+    version="0.5.6",
     author="Tom Leach",
     author_email="tom@leach.it",
     description="A MongoDB object-document mapping layer for Python",
