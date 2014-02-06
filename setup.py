c60e803aed186cce6ae263bb1b370ca7c2ca33b9
Tom Leach <tom@gc.io>
2014-02-06 09:46:33 -0500

    Bump version to 0.6.4. This is a release mainly to solve a pypi issue where the previous release was botched and a "bad" version of 0.6.3 seems to have made it into the wild

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name="Mongothon",
-    version="0.6.3",
+    version="0.6.4",
     author="Tom Leach",
     author_email="tom@gc.io",
     description="A MongoDB object-document mapping layer for Python",
