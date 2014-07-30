7bbdd8b41e6cb2afb64a7b860e51553748a160bc
Tom Leach <tom@gc.io>
2014-07-30 16:01:20 -0400

    Use new version of Schemer and make the version float

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name="Mongothon",
-    version="0.7.16",
+    version="0.7.17",
     author="Tom Leach",
     author_email="tom@gc.io",
     description="A MongoDB object-document mapping layer for Python",
@@ -14,6 +14,6 @@ setuptools.setup(
     long_description="Mongothon is a MongoDB object-document mapping " +
                      "API for Python, loosely based on the awesome " +
                      "mongoose.js library.",
-    install_requires=['pymongo>=2.5.0', 'inflection==0.2.0', 'schemer==0.2.4'],
+    install_requires=['pymongo>=2.5.0', 'inflection==0.2.0', 'schemer>=0.2.0, <0.3.0'],
     tests_require=['mock', 'nose']
     )
